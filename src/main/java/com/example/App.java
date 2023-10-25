b0a523a6238d01a96fef9060d5d228cc5d91c03a
NR <nabilrilo0@gmail.com>
2023-10-25 16:49:14 +0700

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class App {
+ public static void main(String[] args) {
+ String text = "Hello, Maven!";
+ String reversedText = StringUtils.reverse(text);
+ System.out.println(reversedText);
+ }
+ }
